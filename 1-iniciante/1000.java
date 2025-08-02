6716f9dc5a517f23747779d8b4aeb4d0caaeb3a0
João Vitor <vwracity@gmail.com>
2025-08-02 02:00:53 -0300

    Adiciona estrutura do repositório citada no read.md, e o exercicio 1000

---
+++ 1-iniciante/1000.java
@@ -0,0 +1,13 @@
+// Problema 1000 - Hello World!
+// https://judge.beecrowd.com/pt/problems/view/1000
+// Observações: Nenhuma
+
+public class 1000 {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
---
+++ 1-iniciante/1006.java
@@ -0,0 +1,3 @@
+// Problema 1000 - Hello World!
+// https://judge.beecrowd.com/pt/problems/view/1000
+// Observações: Nenhuma
